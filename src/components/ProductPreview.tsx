1aa7ce9d1f0f3bdded7bed7034d54695f1d9e0d2
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2022-09-25 17:24:57 -0500

    Update ProductPreview.tsx

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -4,15 +4,26 @@ import React from 'react';
 import getColorSwatch from '@/utils/get-color-swatch';
 
 const ProductPreview = (product: ShopifyBuy.Product) => {
+  const [currentImage, setCurrentImage] = React.useState(0);
+
   return (
-    <div className="h-[250px] w-[200px] flex-col items-center rounded bg-amber-50 p-4">
+    <div className="relative h-[250px] w-[200px] cursor-pointer flex-col items-center rounded bg-amber-50 p-4 transition-all hover:scale-[1.05]">
+      <div className="absolute top-0 right-0 z-10 rounded bg-amber-200 px-4">
+        <p className="text-center font-semibold uppercase">
+          {product.variants[0]?.price!.split('.')[0]}
+        </p>
+      </div>
       <div className="flex justify-center">
         <Image
-          src={product.images[0]!.src}
+          src={product.images[currentImage]!.src}
           alt={product.title}
           width={150}
           height={150}
           className="mix-blend-multiply"
+          onClick={() =>
+            setCurrentImage((prev) => (prev + 1) % product.images.length)
+          }
+          draggable={false}
         />
       </div>
       <h4 className="font-mono text-sm lowercase">{product.title}</h4>
