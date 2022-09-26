d43730b3775b8691d860a821d9d9f5b053107dc2
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2022-09-26 12:53:19 -0500

    layout

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -11,7 +11,8 @@ const Shop = () => {
 
   const fetchProducts = async () => {
     setLoading(true);
-    const freshProducts = await shopify.product.fetchAll();
+    const freshProducts = await shopify.product.fetchAll(2);
+    console.log(freshProducts.length);
     setProducts(freshProducts);
     setLoading(false);
   };
@@ -26,6 +27,8 @@ const Shop = () => {
         'w-screen rounded-xl border border-amber-50/30 p-8 backdrop-blur-sm transition-all lg:h-[600px] lg:w-[700px] lg:hover:backdrop-blur-md',
         {
           'flex h-[316px] items-center justify-center': loading,
+          'grid grid-cols-1 gap-4 overflow-y-auto md:grid-cols-2 lg:grid-cols-3':
+            !loading,
         }
       )}
     >
